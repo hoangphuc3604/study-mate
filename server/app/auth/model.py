6cb0847fd1617186abc4ebfdf4d0b9fd6e0b686e
22120278 <nvhphuc364@gmail.com>
2025-05-21 12:49:29 +0700

    add auth feat

---
+++ server/app/auth/model.py
@@ -0,0 +1,29 @@
+from app import db
+from datetime import datetime
+from app import bcrypt
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<User {self.fullname}>'
+    
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'fullname': self.fullname,
+            'email': self.email,
+            'created_at': self.created_at
+        }
