8239934adc2c183194f38dbced49b0437e6b05a7
22120278 <nvhphuc364@gmail.com>
2025-05-24 00:04:17 +0700

    add format date helper function

--- client/src/helpers/formatTime.ts
+++ client/src/helpers/formatTime.ts
@@ -1,3 +1,16 @@
-export const formatTimestamp = (date: string) => {
-    return new Date(date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+export const formatTimestamp = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toLocaleTimeString("vi-VN", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+};
+
+export const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("vi-VN", {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    });
 };
